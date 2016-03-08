9e0f4e8fbf200cb8e22295b7605eea5b9ddcefee
afsanchezs <afsanchez96@gmail.com>
2016-03-08 14:46:45 +0000

    Modificación CPU

--- CPU/main.c
+++ CPU/main.c
@@ -1,15 +1,64 @@
-/*
- */
+#include "procesador.h"
+#include "ports.h"
 
-#include <avr/io.h>
+void secuencias(uint8_t sel);
 
-int main(void)
+int main(){
+
+    uint8_t sel;
+
+    CPUInit();
+
+    CPUWrite(DDR_A, 0);
+    CPUWrite(DDR_B, 0xFF);
+
+    while(1){
+        sel = CPURead(PIN_A);
+        secuencias(sel);
+        _delay_ms(5);
+    }
+}
+
+void secuencias(uint8_t sel)
 {
+    uint8_t value;
 
-    // Insert code
+    switch(sel)
+    {
+        case 1:
+            value = CPURead(PORT_B);
+            if(value != 1)
+                value = 1;
+            else
+                value <<= 1;
+            CPUWrite(PORT_B, value);
+            break;
 
-    while(1)
-    ;
+        case 2:
+            value = CPURead(PORT_B);
+            if(value != 0x80)
+                value = 0x80;
+            else
+                value >>= 1;
+            CPUWrite(PORT_B, value);
+            break;
 
-    return 0;
+        case 4:
+            value = CPURead(PORT_B);
+            value++;
+            CPUWrite(PORT_B, value);
+            break;
+
+        case 8:
+            value = CPURead(PORT_B);
+            value--;
+            CPUWrite(PORT_B, value);
+            break;
+
+        default:
+            value = CPURead(PORT_B);
+            value ^= 0xFF;
+            CPUWrite(PORT_B, value);
+            break;
+    }
 }
